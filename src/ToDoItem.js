8ece42667f7dae34a46365a72b8961a30d4c8675
zizi-busy <inka.zr@gmail.com>
2020-03-08 17:56:18 +0200

    files added

---
+++ src/ToDoItem.js
@@ -0,0 +1,13 @@
+import React , { Component} from 'react'
+
+function ToDoItem () {
+    return (
+        <div className="todo-item">
+                <input type="checkbox" />
+                <p>Placeholder Item</p>
+        </div>
+    )
+}
+
+export default ToDoItem
+
